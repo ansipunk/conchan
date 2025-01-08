b1b54caf5483ef82eff094fccef203f7aac4d601
ansipunk <ansipunk@anarchy.team>
2025-01-08 16:27:41 +0500

    Noop

--- bot.ts
+++ bot.ts
@@ -1,6 +1,6 @@
 import { Bot, Context } from "./deps.deno.ts";
 
-const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
+export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
 
 bot.command("start", (ctx: Context) => {
   ctx.reply("Welcome! Up and running.");
@@ -9,5 +9,3 @@ bot.command("start", (ctx: Context) => {
 bot.command("ping", (ctx: Context) => {
   ctx.reply(`Pong! ${new Date()} ${Date.now()}`);
 });
-
-export { bot };
